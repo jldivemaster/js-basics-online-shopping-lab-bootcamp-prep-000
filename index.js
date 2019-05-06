071e522f89d6368792b730ec0c78586d5c3c49a5
Jason Leach <jlspursfan@gmail.com>
2019-05-06 02:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,22 +14,27 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  str = "In your cart, you have ";
+  
 }
 
 function total() {
-  // write your code here
+  total = 0;
+  cart.forEach(item) {
+    total += item[itemPrice];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-
+  
 }
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    console.log("Sorry, we don't have a credit card on file for you.");
   } else {
-    cart.length = 0
-    console.log("Your total cost is $${total()}, which will be charged to the card ${cardNumber}.")
+    cart.length = 0;
+    console.log("Your total cost is $${total()}, which will be charged to the card ${cardNumber}.");
   }
 }
